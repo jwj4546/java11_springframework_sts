df25b755d462906a9e745d1e28c1e632f24fba23
jwj4546 <83587579+jwj4546@users.noreply.github.com>
2024-05-07 17:51:18 +0900

    DI practice1

---
+++ spring3/src/main/java/com/spring3/service/SampleServiceImpl.java
@@ -0,0 +1,43 @@
+package com.spring3.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring3.dao.SampleDAO;
+import com.spring3.dto.Sample;
+
+@Service
+public class SampleServiceImpl implements SampleService {
+
+	@Autowired
+	private SampleDAO sampleDAO;
+	
+	@Override
+	public List<Sample> getSampleList() {
+		return sampleDAO.getSampleList();
+	}
+
+	@Override
+	public Sample getSample(int num) {
+		return sampleDAO.getSample(num);
+	}
+
+	@Override
+	public void insSample(Sample sample) {
+		sampleDAO.insSample(sample);
+	}
+
+	@Override
+	public void upSample(Sample sample) {
+		sampleDAO.upSample(sample);
+	}
+
+	@Override
+	public void delSample(Sample sample) {
+		sampleDAO.delSample(sample);
+	}
+
+	
+}
